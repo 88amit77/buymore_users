944e1b72f5a6fa924d6b8f4ab9aaa09945021f53
raj.t <raj.t@pacewisdom.com>
2020-03-23 18:07:31 +0530

    Updated url endpoits

--- users/api/urls.py
+++ users/api/urls.py
@@ -1,14 +1,19 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
 from rest_framework_simplejwt import views as jwt_views
 from rest_framework_swagger.views import get_swagger_view
-
+from .users import views
 # TODO: add here your API URLs
 
 schema_view = get_swagger_view(title='Micromerce API')
-
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
 
 urlpatterns = [
-	path("users/docs/", schema_view),
-	path('users/login/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
-	path('users/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh')
+	path('', include(router.urls)),
+	path("docs/", schema_view),
+	path('login/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
+	path('refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh')
+
 ]
