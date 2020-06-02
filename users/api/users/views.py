581838a55e49f2a722df144016176a0320c48b55
raj.t <raj.t@pacewisdom.com>
2020-06-02 08:30:39 +0530

    Added username filter

--- users/api/users/views.py
+++ users/api/users/views.py
@@ -179,6 +179,30 @@ class UserIdDataKeywordFilterView(APIView):
         return Response(users)
 
 
+class UserNameDataKeywordFilterView(APIView):
+    """
+    View to list all users in the system.
+
+    * Requires token authentication.
+    * Only admin users are able to access this view.
+    """
+    # authentication_classes = [authentication.TokenAuthentication]
+    # permission_classes = [permissions.IsAdminUser]
+
+    def get(self, request, format=None):
+        """
+        Return a list of all users.
+        """
+        qs = User.objects.all().order_by('id')
+        if 'id' in request.data:
+            ids = request.data['id'].split(',')
+            qs = qs.filter(id__in=ids)
+        if 'keyword' in request.data:
+            qs = qs.filter(username__contains=request.data['keyword'])
+        users = [{user.id: user.id} for user in qs]
+        return Response(users)
+
+
 class DepartmentViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows groups to be viewed or edited.
